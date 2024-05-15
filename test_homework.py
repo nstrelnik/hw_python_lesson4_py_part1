dc25f321f40812852d03a4660acaeb9b6570c048
Anastasia Strelnik <n.strelnik@mail.ru>
2024-05-15 18:22:44 +0200

    Добавлены расчеты

--- test_homework.py
+++ test_homework.py
@@ -8,7 +8,7 @@ def test_greeting():
     """
     name = "Анна"
     age = 25
-    # TODO Сформируйте нужную строку
+
     output = f"Привет, {name}! Тебе {age} лет."
 
     # Проверяем результат
@@ -23,12 +23,11 @@ def test_rectangle():
     """
     a = 10
     b = 20
-    # TODO сосчитайте периметр
+
     perimeter = (a + b) * 2
 
     assert perimeter == 60
 
-    # TODO сосчитайте площадь
     area = a * b
 
     assert area == 200
@@ -40,12 +39,11 @@ def test_circle():
     Используйте константу PI
     """
     r = 23
-    # TODO сосчитайте площадь
+
     area = math.pi * r ** 2
 
     assert area == 1661.9025137490005
 
-    # TODO сосчитайте длину окружности
     length = (2 * math.pi) * r
 
     assert length == 144.51326206513048
@@ -55,7 +53,7 @@ def test_random_list():
     """
     Создайте список из 10 случайных чисел от 1 до 100 (включая обе границы) и отсортируйте его по возрастанию.
     """
-    # TODO создайте список
+
     l = sorted([random.randint(1, 100) for _ in range(10)])
     #l = sorted(random.sample(range(1, 101), 10))
 
@@ -70,7 +68,6 @@ def test_unique_elements():
     """
     l = [1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 8, 9, 10, 10]
     l = list(set(l))
-    # TODO удалите повторяющиеся элементы
 
     assert isinstance(l, list)
     assert len(l) == 10
@@ -86,7 +83,7 @@ def test_dicts():
     """
     first = ["a", "b", "c", "d", "e"]
     second = [1, 2, 3, 4, 5]
-    # TODO создайте словарь
+
     d = dict(zip(first, second))
 
     assert isinstance(d, dict)
